ed65e8644fb3c4d920e3a6d31e4c81bf84df2242
wangsiyang68 <siyang.official@gmail.com>
2024-02-15 10:47:44 +0800

    refactor: separate form and leaderboard

--- app/leaderboard/page.js
+++ app/leaderboard/page.js
@@ -1,25 +1,9 @@
 "use client"; //this is a client-side module https://stackoverflow.com/questions/74965849/youre-importing-a-component-that-needs-usestate-it-only-works-in-a-client-comp
 
 import Link from 'next/link'
-import { db } from './firebaseConfig'
+import { db } from '../firebaseConfig'
 import { collection, getDocs, addDoc } from 'firebase/firestore';
 import React, { useState, useEffect } from 'react';
-
-// Add document to collection
-async function addData(teamName, score) {
-    try {
-        const colRef = collection(db, "Highscore");
-        const docRef = await addDoc(colRef, {
-            TeamName: teamName,
-            Score: score
-        });
-        console.log("Document written with ID: ", docRef.id);
-        return true;
-    } catch (error) {
-        console.error("Error adding document: ", error.message);
-        return false;
-    }
-}
 
 // Get documents from collection
 async function fetchData() {
@@ -33,8 +17,6 @@ async function fetchData() {
 }
 
 export default function Leaderboard() {
-    const [teamName, setTeamName] = useState('');
-    const [score, setScore] = useState(0);
     const [userData, setUserData] = useState([]);
 
     useEffect(() => {
@@ -45,34 +27,9 @@ export default function Leaderboard() {
         fetchDataFromFirestore();
     }, []);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const added = await addData(teamName, score);
-        if (added) {
-            setTeamName('');
-            setScore('');
-            console.log(' Data added successfully');
-        }
-    };
     return (
         <main>
             <h1>Leaderboard</h1>
-
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Team Name"
-                    value={teamName}
-                    onChange={(e) => setTeamName(e.target.value)}
-                />
-                <input
-                    type="number"
-                    placeholder="Score"
-                    value={score}
-                    onChange={(e) => setScore(e.target.value)}
-                />
-                <button type="submit">Submit</button>
-            </form>
 
             <div>
                 {userData.map((data) => (
--- app/newgame/page.js
+++ app/newgame/page.js
@@ -1,12 +1,62 @@
+"use client"; //this is a client-side module https://stackoverflow.com/questions/74965849/youre-importing-a-component-that-needs-usestate-it-only-works-in-a-client-comp
+
 import Link from 'next/link'
+import { db } from '../firebaseConfig'
+import { collection, getDocs, addDoc } from 'firebase/firestore';
+import React, { useState, useEffect } from 'react';
+
+// Add document to collection
+async function addData(teamName, score) {
+    try {
+        const colRef = collection(db, "Highscore");
+        const docRef = await addDoc(colRef, {
+            TeamName: teamName,
+            Score: score
+        });
+        console.log("Document written with ID: ", docRef.id);
+        return true;
+    } catch (error) {
+        console.error("Error adding document: ", error.message);
+        return false;
+    }
+}
 
 export default function NewGame() {
+    const [teamName, setTeamName] = useState('');
+    const [score, setScore] = useState(0);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const added = await addData(teamName, score);
+        if (added) {
+            setTeamName('');
+            setScore('');
+            console.log(' Data added successfully');
+        }
+    };
     return (
-        <>
+        <main>
             <h1>New Game</h1>
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Team Name"
+                    value={teamName}
+                    onChange={(e) => setTeamName(e.target.value)}
+                />
+                <input
+                    type="number"
+                    placeholder="Score"
+                    value={score}
+                    onChange={(e) => setScore(e.target.value)}
+                />
+                <button type="submit">Submit</button>
+            </form>
+
             <h2>
                 <Link href="/">Back to home</Link>
             </h2>
-        </>
+        </main>
     );
 }
