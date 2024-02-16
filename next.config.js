8483e45039647a3e79dad1232072b5142cbe20bf
sihvn <77093664+sihvn@users.noreply.github.com>
2024-02-17 01:06:48 +0800

    for image loading

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/images',
     reactStrictMode: true,
     // rewrites: async () => {
     //     return [
