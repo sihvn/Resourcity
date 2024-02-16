40a25698b5e0be7f8fcdde23b27114230fd9e0c2
sihvn <77093664+sihvn@users.noreply.github.com>
2024-02-17 01:24:10 +0800

    image loading test 2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/CapstoneProject',
     reactStrictMode: true,
     // rewrites: async () => {
     //     return [
