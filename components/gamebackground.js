51e29ead928805791a23640d5b63e8d6a6c2b9f8
wangsiyang68 <siyang.official@gmail.com>
2024-02-24 18:06:00 +0800

    chore: summarize gamebackground txt

--- components/gamebackground.js
+++ components/gamebackground.js
@@ -9,11 +9,9 @@ const GameBackground = () => {
             </div>
             <div className="text-left p-8">
                 <h1 className="text-4xl font-bold mb-4">The city needs your help! </h1>
-                <p className="text-lg">As climate issues and overconsumption worsen around the world, countries have started to keep resources to themselves. </p>
-                <p className="text-lg">Imports into the city have ceased, which is a major problem for the city that has historically relied heavily on imports for survival. </p>
-                <p className="text-lg">As fellow citizens, take charge and use resources to redesign your city to ensure that it is self-sustaining and able to withstand any incoming crises.  </p>
-                <p className="text-lg">To win, your team must survive 4 Crisis situations. </p>
-                <p className="text-lg">You lose if your team runs out of any Resource type at any point during the game. </p>
+                <p className="text-lg">Due to global climate issues and overconsumption, countries are keeping resources internally. This cessation of imports poses a significant challenge for a city reliant on them for survival.</p>
+                <br></br>
+                <p className="text-lg">As citizens, redesign the city to be self-sustaining and resilient to crises. To win, survive <b>four crises</b>, but losing occurs if any resource type runs out.</p>
                 <br></br>
                 <Link href="/rules" className="text-red hover:text-gray-500 cursor-pointer">Learn More..</Link>
             </div>
