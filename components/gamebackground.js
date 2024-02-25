7ab9070e9f88808b18708edc73382d803c1b965d
sihvn <77093664+sihvn@users.noreply.github.com>
2024-02-25 12:08:41 +0800

    adjusted homepage for mobile device

--- components/gamebackground.js
+++ components/gamebackground.js
@@ -7,11 +7,20 @@ const GameBackground = () => {
             <div className="flex justify-center items-center">
                 <img src="/CapstoneProject/images/Backgroundimage.png" />
             </div>
-            <div className="text-left p-8">
+            <div className="text-left p-4">
                 <h1 className="text-4xl font-bold mb-4">The city needs your help! </h1>
                 <p className="text-lg">Due to global climate issues and overconsumption, countries are keeping resources internally. This cessation of imports poses a significant challenge for a city reliant on them for survival.</p>
                 <br></br>
-                <p className="text-lg">As citizens, redesign the city to be self-sustaining and resilient to crises. To win, survive <b>four crises</b>, but losing occurs if any resource type runs out.</p>
+                <h1 className="text-4xl font-bold mb-4">How to play </h1>
+                <p className="text-lg">As fellow citizens, your team decides to take action to help your city become self-sustaining. </p>
+                <br></br>
+                <p className="text-lg">Work together to transform the board by Rotating and Replacing tiles. Connect roads and form Farms, then Plant Crops in exchange for Resources to replenish the Resource Pool! </p>
+                <br></br>
+                <p className="text-lg">But be careful - every Action you take consumes Resources, and a Crisis might strike at any time! Can your team create a truly resilient city without letting the Resource Pool run out?</p>
+                <br></br>
+                <h1 className="text-4xl font-bold mb-4">How to win </h1>
+                <p className="text-lg">To win, your team must survive 4 Crisis situations. </p>
+                <p className="text-lg">You lose if your team runs out of any Resource type at any point during the game. </p>
                 <br></br>
                 <Link href="/rules" className="text-red hover:text-gray-500 cursor-pointer">Learn More..</Link>
             </div>
--- components/playeraction.js
+++ components/playeraction.js
@@ -10,67 +10,67 @@ const PlayerAction = () => {
                 <p className="text-lg">You may take up to 4 Actions each turn. </p>
                 <p className="text-lg">Select any combination of the Actions listed below. You may do the same Action several times, each time counting as 1 Action. Your Superpower may change how an Action is done.  </p>
             </div>
-            <div className="flex justify-between pb-8">
-                <div className="w-1/3 p-10">
-                    <img src="/CapstoneProject/images/playermove1.jpg" alt="Image 1" className="w-full h-auto mb-4" />
-                    <h1 className="text-xl font-bold mb-4">Move</h1>
-                    <p>Move to an adjacent tile, following the road.</p>
-                    <p>You cannot move to an adjacent tile if its road is disconnected from your current tile. </p>
-                    <p>Moving by 1 tile counts as 1 Action. </p>
-                    <p>A maximum of 3 players can be on 1 tile together.</p>
-                    <p className="font-bold">Cost: Free!</p>
-                </div>
-
-                <div className="w-1/3 p-10">
-                    <img src="/CapstoneProject/images/playermove2.jpg" alt="Image 2" className="w-full h-auto mb-4" />
-                    <h1 className="text-xl font-bold mb-4">Exchange </h1>
-                    <p>If standing on the same tile as another player, you may exchange a tile from each of your hands. </p>
-                    <p className="font-bold">Cost: Free!</p>
-                </div>
-                <div className="w-1/3 p-10">
-                    <img src="/CapstoneProject/images/playermove3.jpg" alt="Image 3" className="w-full h-auto mb-4" />
-                    <h1 className="text-xl font-bold mb-4">Discard </h1>
-                    <p>Discard any tile from your hand into the Discard Pile. Draw 1 tile immediately. </p>
-                    <p className="font-bold">Cost: Free!</p>
-                </div>
-                <div className="w-1/3 p-10">
-                    <img src="/CapstoneProject/images/playermove3.jpg" alt="Image 3" className="w-full h-auto mb-4" />
-                    <h1 className="text-xl font-bold mb-4">Rotate </h1>
-                    <p>Rotate any adjacent tile by 90 degrees. </p>
-                    <p className="font-bold">Cost: 1 fuel</p>
-                </div>
+            {/* <div className="flex justify-between pb-8"> */}
+            <div className="w-1/3 p-10">
+                <img src="/CapstoneProject/images/playermove1.jpg" alt="Image 1" className="w-full h-auto mb-4" />
+                <h1 className="text-xl font-bold mb-4">Move</h1>
+                <p>Move to an adjacent tile, following the road.</p>
+                <p>You cannot move to an adjacent tile if its road is disconnected from your current tile. </p>
+                <p>Moving by 1 tile counts as 1 Action. </p>
+                <p>A maximum of 3 players can be on 1 tile together.</p>
+                <p className="font-bold">Cost: Free!</p>
             </div>
-            <div className="flex justify-between">
-                <div className="w-1/3 p-10">
-                    <img src="/CapstoneProject/images/playermove1.jpg" alt="Image 1" className="w-full h-auto mb-4" />
-                    <h1 className="text-xl font-bold mb-4">Swap</h1>
-                    <p>Swap positions with another player. </p>
-                    <p>To do this, move your player token AND the tile you are standing on.</p>
-                    <p>Exchange positions with another player’s token AND the tile they are standing on. </p>
-                    <p className="font-bold">Cost: 2 fuel</p>
-                </div>
 
-                <div className="w-1/3 p-10">
-                    <img src="/CapstoneProject/images/playermove2.jpg" alt="Image 2" className="w-full h-auto mb-4" />
-                    <h1 className="text-xl font-bold mb-4">Use Super-power </h1>
-                    <Link href="/superpower" className="text-red hover:text-gray-500 cursor-pointer">See Superpower Cards</Link>
-                    <p className="font-bold">Cost: Free!</p>
-                </div>
-                <div className="w-1/3 p-10">
-                    <img src="/CapstoneProject/images/playermove3.jpg" alt="Image 3" className="w-full h-auto mb-4" />
-                    <h1 className="text-xl font-bold mb-4">Farm </h1>
-                    <p>Build, Plant, Harvest, Fertilize</p>
-                    <Link href="/farmactions" className="text-red hover:text-gray-500 cursor-pointer">See farm actions</Link>
-                </div>
-                <div className="w-1/3 p-10">
-                    <img src="/CapstoneProject/images/playermove3.jpg" alt="Image 3" className="w-full h-auto mb-4" />
-                    <h1 className="text-xl font-bold mb-4">Replace </h1>
-                    <p> Choose a tile from your hand to Replace the tile you are standing on, or any adjacent tile (including Community Centre tiles).  </p>
-                    <p>You may Rotate the tile while placing it. Roads do not need to make sense (be continuous). </p>
-                    <p>Draw a new tile only at the end of your turn. </p>
-                    <p className="font-bold">Cost: 3 fuel</p>
-                </div>
+            <div className="w-1/3 p-10">
+                <img src="/CapstoneProject/images/playermove2.jpg" alt="Image 2" className="w-full h-auto mb-4" />
+                <h1 className="text-xl font-bold mb-4">Exchange </h1>
+                <p>If standing on the same tile as another player, you may exchange a tile from each of your hands. </p>
+                <p className="font-bold">Cost: Free!</p>
             </div>
+            <div className="w-1/3 p-10">
+                <img src="/CapstoneProject/images/playermove3.jpg" alt="Image 3" className="w-full h-auto mb-4" />
+                <h1 className="text-xl font-bold mb-4">Discard </h1>
+                <p>Discard any tile from your hand into the Discard Pile. Draw 1 tile immediately. </p>
+                <p className="font-bold">Cost: Free!</p>
+            </div>
+            <div className="w-1/3 p-10">
+                <img src="/CapstoneProject/images/playermove3.jpg" alt="Image 3" className="w-full h-auto mb-4" />
+                <h1 className="text-xl font-bold mb-4">Rotate </h1>
+                <p>Rotate any adjacent tile by 90 degrees. </p>
+                <p className="font-bold">Cost: 1 fuel</p>
+            </div>
+            {/* </div> */}
+            {/* <div className="flex justify-between"> */}
+            <div className="w-1/3 p-10">
+                <img src="/CapstoneProject/images/playermove1.jpg" alt="Image 1" className="w-full h-auto mb-4" />
+                <h1 className="text-xl font-bold mb-4">Swap</h1>
+                <p>Swap positions with another player. </p>
+                <p>To do this, move your player token AND the tile you are standing on.</p>
+                <p>Exchange positions with another player’s token AND the tile they are standing on. </p>
+                <p className="font-bold">Cost: 2 fuel</p>
+            </div>
+
+            <div className="w-1/3 p-10">
+                <img src="/CapstoneProject/images/playermove2.jpg" alt="Image 2" className="w-full h-auto mb-4" />
+                <h1 className="text-xl font-bold mb-4">Use Super-power </h1>
+                <Link href="/superpower" className="text-red hover:text-gray-500 cursor-pointer">See Superpower Cards</Link>
+                <p className="font-bold">Cost: Free!</p>
+            </div>
+            <div className="w-1/3 p-10">
+                <img src="/CapstoneProject/images/playermove3.jpg" alt="Image 3" className="w-full h-auto mb-4" />
+                <h1 className="text-xl font-bold mb-4">Farm </h1>
+                <p>Build, Plant, Harvest, Fertilize</p>
+                <Link href="/farmactions" className="text-red hover:text-gray-500 cursor-pointer">See farm actions</Link>
+            </div>
+            <div className="w-1/3 p-10">
+                <img src="/CapstoneProject/images/playermove3.jpg" alt="Image 3" className="w-full h-auto mb-4" />
+                <h1 className="text-xl font-bold mb-4">Replace </h1>
+                <p> Choose a tile from your hand to Replace the tile you are standing on, or any adjacent tile (including Community Centre tiles).  </p>
+                <p>You may Rotate the tile while placing it. Roads do not need to make sense (be continuous). </p>
+                <p>Draw a new tile only at the end of your turn. </p>
+                <p className="font-bold">Cost: 3 fuel</p>
+            </div>
+            {/* </div> */}
             <div className="mb-8 text-left  border-b-2 pb-8">
                 <h1 className="text-xl font-bold mb-4">At the end of your turn, move 1 Food and 1 Water token to the Used Pile. </h1>
                 <p className="text-lg">If you have 3 tiles left in your hand (ie. did not use any tiles in your hand), you must discard 1 tile into the Discard Pile. Draw 1 new tile immediately.</p>
