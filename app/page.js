72d4e79f255e5fb2c8904f623c9f330b453c1ed9
wangsiyang68 <siyang.official@gmail.com>
2024-05-18 12:40:35 +0800

    feat: dynamically resize video to viewport width

--- app/page.js
+++ app/page.js
@@ -4,15 +4,37 @@ import BaseTemplate from '../components/baseTemplate';
 import GameBackground from '../components/gamebackground';
 import Navbar from '../components/navbar';
 import Footer1 from '../components/footer';
+import { useEffect, useState } from 'react';
 
 export default function Page() {
+    const [dimensions, setDimensions] = useState({
+        width: '80%',
+        height: '100%'
+    });
+
+    // useEffect(() => {
+    //     const handleResize = () => {
+    //         setDimensions({
+    //             width: '100%',
+    //             height: '100%'
+    //         });
+    //     };
+
+    //     window.addEventListener('resize', handleResize);
+
+    //     // Cleanup event listener on component unmount
+    //     return () => {
+    //         window.removeEventListener('resize', handleResize);
+    //     };
+    // }, []);
+
     return (
         <>
             <Navbar />
             <div className='flex items-center justify-center'>
                 <iframe
-                    width='1280'
-                    height='720'
+                    style={{ width: dimensions.width, height: dimensions.height }}
+
                     src="https://www.youtube.com/embed/8UWY_CAm3x8"
                     frameborder="0"
                     allowfullscreen
