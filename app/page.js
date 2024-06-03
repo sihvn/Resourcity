2527101779b89d9eefaf15b4241612e5bb7f7bee
wangsiyang68 <siyang.official@gmail.com>
2024-06-03 23:45:57 +0800

    fix: adjust video widths and heights

--- app/page.js
+++ app/page.js
@@ -7,34 +7,41 @@ import Footer1 from '../components/footer';
 import { useEffect, useState } from 'react';
 import { Layout } from 'antd';
 
+//TODO: fix winodw not defined error with https://stackoverflow.com/questions/75692116/next-js-13-window-is-not-defined
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+        width,
+        height
+    };
+}
+
+function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowDimensions;
+
+}
+
 export default function Page() {
-    const [dimensions, setDimensions] = useState({
-        width: '80%',
-        height: '100%'
-    });
 
-    // useEffect(() => {
-    //     const handleResize = () => {
-    //         setDimensions({
-    //             width: '100%',
-    //             height: '100%'
-    //         });
-    //     };
-
-    //     window.addEventListener('resize', handleResize);
-
-    //     // Cleanup event listener on component unmount
-    //     return () => {
-    //         window.removeEventListener('resize', handleResize);
-    //     };
-    // }, []);
+    const { height, width } = useWindowDimensions();
 
     return (
         <Layout>
             <Navbar />
             <div className='flex items-center justify-center'>
                 <iframe
-                    style={{ width: dimensions.width, height: dimensions.height }}
+                    style={{ width: width, height: height * 0.4 }}
                     src="https://www.youtube.com/embed/KpgShENMN90"
                     frameBorder="0"
                     allowFullScreen
